de065abbd2c048ef4d0c1bcefc1fb3d28270e54e
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-06 00:25:11 -0300

    Feat: Contagem no Header

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    bracketSameLine: false,
+    trailingComma: "all",
+    arrowParens: "avoid",
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,38 +6,38 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+    function handleAddTask(newTaskTitle: string) {
+        //TODO - add new task
+    }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+    function handleToggleTaskDone(id: number) {
+        //TODO - toggle task done if exists
+    }
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+    function handleRemoveTask(id: number) {
+        //TODO - remove task from state
+    }
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+    return (
+        <View style={styles.container}>
+            <Header tasksCounter={tasks.length} />
 
-      <TodoInput addTask={handleAddTask} />
+            <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
+            <TasksList
+                tasks={tasks}
+                toggleTaskDone={handleToggleTaskDone}
+                removeTask={handleRemoveTask}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    container: {
+        flex: 1,
+        backgroundColor: '#EBEBEB',
+    },
+});
